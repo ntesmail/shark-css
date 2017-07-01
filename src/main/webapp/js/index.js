9ff944289a0dd5da776a27a22bfc02b49058f1e5
whw <m15990176834@163.com>
2017-07-01 16:17:29 +0800

    add: shark-css重构

--- src/main/webapp/js/index.js
+++
@@ -1,3 +0,0 @@
-$('.bs-example').each(function(index, ele) {
-    $(ele).after('<pre><code class="language-markup">' + ele.innerHTML.replace(/&/ig, '&amp;').replace(/</ig, '&lt;').replace(/>/ig, '&gt;').trim() + '</code></pre>');
-})
